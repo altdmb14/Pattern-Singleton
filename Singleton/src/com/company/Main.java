f4a7b1ea4dc0b47bc65d956343d6caad73539085
altdmb14 <altdmb14@gmail.com>
2020-05-04 22:52:14 +0500

    Написал паттерн Сигнлетон

---
+++ Singleton/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        ProgramLogger.getProgramLogger().addLogInfo("Firsl logg...");
+        ProgramLogger.getProgramLogger().showLogFile();
+    }
+}
